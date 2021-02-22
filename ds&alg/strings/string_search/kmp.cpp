79e44815b7f3a0477778d62aa08029b6b16b37b7
Fangwen Wu <hzwufangwen@gmail.com>
2021-02-22 20:23:31 +0800

    fix kmp implementation bug

--- ds&alg/strings/string_search/kmp.cpp
+++ ds&alg/strings/string_search/kmp.cpp
@@ -2,12 +2,13 @@
 
 vector<int> preprocess(string s){
     int n = s.size();
-    vector<int> v(n,);
+    vector<int> v(n,0);
+    v[0] = -1;
     int j = -1;
 
     for(int i = 1;i < n; i++){
         //next not equal, go to current v[i]
-        while(j>=0 && s[i] != s[j+1]) j = v[i];
+        while(j>=0 && s[i] != s[j+1]) j = v[j];
         if(s[i] == s[j+1]) j++;
         v[i] = j;
     }
@@ -15,17 +16,20 @@ vector<int> preprocess(string s){
     return v;
 }
 
-int KMP(string pattern, string s) {
-    int m = pattern.size();
-    int n = s.size();
+int KMP(string haystack, string needle) {
+    if(needle.size()==0){
+        return 0;
+    }
 
-    vector<int> lps= preprocess(pattern);
+    int m = needle.size();
+    int n = haystack.size();
+
+    vector<int> lps= preprocess(needle);
     int j = -1;
 
     for(int i = 0; i < n; i++){
-        while(j>=0 && s[i] != pattern[j+1]) j = lps[i];
-        if(s[i] == pattern[j+1]) j++;
-        
+        while(j>=0 && haystack[i] != needle[j+1]) j = lps[j];
+        if(haystack[i] == needle[j+1]) j++;
         if(j == m-1) return i-m+1;
     }
 
